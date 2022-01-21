feb17c0fa40bbf2fefddde25eb4cd686d61804e2
Eyan Yehya <eyanyehya@gmail.com>
2022-01-21 18:07:46 -0500

    final commit

--- medicine_project/settings.py
+++ medicine_project/settings.py
@@ -136,8 +136,8 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static'), os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
