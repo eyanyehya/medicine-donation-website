1c8bc267d806646ca551cc065907e685f85f3fcf
Eyan Yehya <eyanyehya@gmail.com>
2022-01-21 23:23:43 -0500

    final removed secret info

--- medicine_project/settings.py
+++ medicine_project/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECCRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 CLOUDINARY_NAME=os.getenv("CLOUDINARY_NAME")
 CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
 CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
